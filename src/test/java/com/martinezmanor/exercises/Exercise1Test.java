835b9984950781ee61f5dce9040bf1f03a536e01
Matthew Martinez <raphobe@users.noreply.github.com>
2020-10-31 01:02:55 +0000

    making junit work with this project

--- src/test/java/com/martinezmanor/exercises/Exercise1Test.java
+++ src/test/java/com/martinezmanor/exercises/Exercise1Test.java
@@ -1,6 +1,12 @@
 package com.martinezmanor.exercises;
 
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class Exercise1Test {
     
+    @Test
+    public void testSomeBehavior() {
+        assertEquals(true, true);
+    }
 }
